4b89e1870778d53f2c65bd98929413746d719469
Kenchiki-chiki <kodama303@gmail.com>
2021-03-07 16:20:02 +0900

    Ideaモデルのテストコードを記述

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with category_id, body" do
+    category = FactoryBot.create(:category)
+    idea = category.ideas.new(
+      category_id: 1,
+      body: "TestTool"
+    )
+    expect(idea).to be_valid
+  end
+
+  it "is invalid without a category_id" do
+    idea = Idea.new(category_id: nil)
+    idea.valid?
+    expect(idea.errors[:category_id]).to include("can't be blank")
+  end
+
+  it "is invalid without a body" do
+    idea = Idea.new(body: nil)
+    idea.valid?
+    expect(idea.errors[:body]).to include("can't be blank")
+  end
 end

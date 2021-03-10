7cceb82e24676657da456c83ca7cf349e1e094ad
Kenchiki-chiki <kodama303@gmail.com>
2021-03-10 22:47:27 +0900

    テストコードの追加

--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -16,11 +16,36 @@ RSpec.describe "Ideas", type: :request do
       expect(response).to have_http_status(:success)
     end
 
+    it 'does not creates a category when there is no category_name' do
+      category = FactoryBot.create(:category, name:'App')      
+        post api_v1_ideas_path, params: {
+            category_name: "",
+            body: "TestTool"
+        }
+      expect(response.status).to eq(422)
+    end
+
+    it 'creates a category when there is no body' do
+      category = FactoryBot.create(:category, name:'App')      
+        post api_v1_ideas_path, params: {
+            category_name: "App",
+            body: ""
+        }
+      expect(response.status).to eq(422)
+    end
+
     it "get request successfully" do
       get api_v1_ideas_path   
       expect(response).to have_http_status(:ok)
-    end
+    end   
 
+    it 'get request successfully when the category name exists' do
+      category = FactoryBot.create(:category, name:'App')      
+        get api_v1_ideas_path, params: {
+            category_name: "App",
+        }
+        expect(response).to have_http_status(:success)
+    end
 
   end
 end
